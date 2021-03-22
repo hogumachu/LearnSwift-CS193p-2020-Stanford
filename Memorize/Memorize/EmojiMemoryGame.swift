0d5808bad3759c3f134664f973352cf45e62cc5a
hogumachu <castle_jun@daum.net>
2021-03-22 20:12:47 +0900

    Lecture 2: MVVM and the Swift Type System

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by 홍성준 on 2021/03/22.
+//
+
+// ViewModel
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃", "🕷"]
+        
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+        
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: -Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
+
