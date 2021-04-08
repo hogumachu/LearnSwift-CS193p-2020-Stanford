69de5e57831035cbfcb6ab141c2cc6aa84f7ca55
hogumachu <castle_jun@daum.net>
2021-04-08 15:09:48 +0900

    Revert "Lecture 6: Animation"
    This reverts commit 7f1488f6c727634921ea35e3c396b77f5f4963cd.

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -31,9 +31,5 @@ class EmojiMemoryGame: ObservableObject {
     func choose(card: MemoryGame<String>.Card) {
         model.choose(card: card)
     }
-    
-    func resetGame() {
-        model = EmojiMemoryGame.createMemoryGame()
-    }
 }
 
